b85e30e33e3661ae3f5a1c8d2f6a8444f7a4b13a
Gaurish Naik <nerdgaurish@gmail.com>
2020-03-18 12:58:47 +0530

    added team slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+$('.slider').slick({
 
+  
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        arrows: false,
+       
+       
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+       
+        
+        slidesToShow: 1
+      }
+    }
+  ]
+});
